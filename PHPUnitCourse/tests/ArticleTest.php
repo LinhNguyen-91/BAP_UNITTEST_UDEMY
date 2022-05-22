83899e8c9e24d4efa03edae3f8aa27ad3d19994b
linhnd <linhnd@bap.jp>
2022-05-22 16:21:55 +0700

    Add tests and refactor code until the tests pass

--- PHPUnitCourse/tests/ArticleTest.php
+++ PHPUnitCourse/tests/ArticleTest.php
@@ -4,17 +4,32 @@ use PHPUnit\Framework\TestCase;
 
 class ArticleTest extends TestCase
 {
+    protected $article;
+
+    protected function setUp(): void
+    {
+
+        $this->article = new App\Article;
+        
+    }
     public function testTitleIsEmptyByDefault()
     {
-        $article = new App\Article;
 
-        $this->assertEmpty($article->title);
+        $this->assertEmpty($this->article->title);
+
     }
 
     public function testSlugIsEmptyWithNoTitle()
     {
-        $article = new App\Article;
 
-        $this->assertEquals($article->getSlug(), "");
+        $this->assertEquals($this->article->getSlug(), "");
+
+    }
+
+    public function testSlugHasSpacesReplacedByUnderscores()
+    {
+        $this->article->title = "An example article";
+
+        $this->assertEquals($this->article->getSlug(), "An_example_article");
     }
 }
